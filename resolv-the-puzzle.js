9cfa35166afb442a23db6bf0856c4eeb3c6e88fa
sergio <devnull@localhost>
2012-04-05 00:06:12 +0200

    Optimizado el dibujado.
    Doble buffering.

--- resolv-the-puzzle.js
+++ resolv-the-puzzle.js
@@ -115,6 +115,8 @@ function Board() {
 // Creating stuff.
 var gameScreen = document.getElementById('gameScreen'); gameScreen.width = 540; gameScreen.height = 540;
 var screen = gameScreen.getContext('2d');
+var bufferCanvas = document.createElement('canvas'); bufferCanvas.width = gameScreen.width; bufferCanvas.height = gameScreen.height;
+var bufferContext = bufferCanvas.getContext('2d');
 var mouse = new MouseListener();
 var squareMoving = null;
 var target = null;
@@ -159,28 +161,30 @@ function game() {
 
   lblInfo.innerHTML = 'Game started<br>Time: '+time+'s<br>Movements: '+movements;
 
-  gameScreen.width = gameScreen.width;
+  bufferContext.fillStyle = '#FFFFFF';
+  bufferContext.fillRect(0, 0, bufferCanvas.width, bufferCanvas.height);
   for (i = 0, y = 0; y < 3; y++)
    for (x = 0; x < 3; x++, i++) {
-    screen.drawImage(bird, board.points[x], board.points[y], 180, 180, board.squares[i].xPos, board.squares[i].yPos, 180, 180);
-    if (x == 0 && y == 0) screen.drawImage(nullSquare, 0, 0);
-    screen.beginPath();
-    screen.rect(board.squares[i].xPos+1, board.squares[i].yPos+1, 180, 180);
-    screen.lineWidth = 1;
-    screen.strokeStyle = "#0000FF";
-    screen.stroke();
+    bufferContext.drawImage(bird, board.points[x], board.points[y], 180, 180, board.squares[i].xPos, board.squares[i].yPos, 180, 180);
+    if (x == 0 && y == 0) bufferContext.drawImage(nullSquare, 0, 0);
+    bufferContext.beginPath();
+    bufferContext.lineWidth = 1;
+    bufferContext.strokeStyle = "#0000FF";
+    bufferContext.rect(board.squares[i].xPos+1, board.squares[i].yPos+1, 180, 180);
+    bufferContext.stroke();
    }
  } else {
   gameState = 'end';
   lblInfo.innerHTML += '<br>Puzzle complete.<br>Reload the page to start again.';
   document.getElementById('lblEndGame').className = '';
-  screen.drawImage(bird, 0, 0);
+  bufferContext.drawImage(bird, 0, 0);
  }
 }
 
 function gameLoop() {
- if (gameState == 'begin') screen.drawImage(dark, 0, 0);
+ if (gameState == 'begin') bufferContext.drawImage(dark, 0, 0);
  else if (gameState == 'game') game();
  else { clearInterval(gameLoopId); clearInterval(timeIntervalId); }
+ screen.drawImage(bufferCanvas, 0, 0);
 }
 
