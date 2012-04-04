531d7bca3efdb44ebc1a7910ac3ca9d75d21a5e8
sergio <devnull@localhost>
2012-04-05 00:36:16 +0200

    Optimizado bucle principal.
    -requestAnimationFrame

--- resolv-the-puzzle.js
+++ resolv-the-puzzle.js
@@ -1,3 +1,26 @@
+// Bucle optimizado para animaciones web.
+(function() {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+   window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+   window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame)
+   window.requestAnimationFrame = function(callback, element) {
+    var currTime = new Date().getTime();
+    var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+   };
+
+  if (!window.cancelAnimationFrame)
+   window.cancelAnimationFrame = function(id) { clearTimeout(id); };
+}());
+
 // Class MouseListener.
 function MouseListener() {
  // Properties.
@@ -184,9 +207,10 @@ function game() {
 }
 
 function gameLoop() {
+ gameLoopId = window.requestAnimationFrame(gameLoop);
  if (gameState == 'begin') bufferContext.drawImage(dark, 0, 0);
  else if (gameState == 'game') game();
- else { clearInterval(gameLoopId); clearInterval(timeIntervalId); }
+ else { window.cancelAnimationFrame(gameLoopId); clearInterval(timeIntervalId); }
  screen.drawImage(bufferCanvas, 0, 0);
 }
 
